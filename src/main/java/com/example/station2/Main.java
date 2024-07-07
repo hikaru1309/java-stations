1d9ee1d6c7a41b0dd281242fa0e5924c6fd48586
hikaru1309 <hikarishiro2001@icloud.com>
2024-07-07 12:04:29 +0900

    clear: station2

--- src/main/java/com/example/station2/Main.java
+++ src/main/java/com/example/station2/Main.java
@@ -3,6 +3,12 @@ package com.example.station2;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        boolean sinjitu = false;
+        System.out.println(sinjitu);
+        int number = 42;
+        System.out.println(number);
+        double pi = 3.141592;
+        System.out.println(pi);
         // ここまで
     }
 }
