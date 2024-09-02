d1527c0cfb9401268944ea9e085492c3bb264a88
hikaru1309 <hikarishiro2001@icloud.com>
2024-09-02 11:58:16 +0900

    clear: staion4.5

--- src/main/java/com/example/station4/Main.java
+++ src/main/java/com/example/station4/Main.java
@@ -3,6 +3,12 @@ package com.example.station4;
 public class Main {
     public static void main(String[] args) {
         // ここから
+        int x = 18 * 28;
+        int y = 24 * 21;
+        float a = 3.141592f;
+        float b = 21.9f / 7;
+        System.out.println(x==y);
+        System.out.println(a<b);
         // ここまで
     }
 }
