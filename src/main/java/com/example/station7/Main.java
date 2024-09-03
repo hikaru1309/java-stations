b91cd98ad4c5cb3e1dbebbf710a7ee5970c5bd58
hikaru1309 <hikarishiro2001@icloud.com>
2024-09-03 11:25:28 +0900

    clear: station7

--- src/main/java/com/example/station7/Main.java
+++ src/main/java/com/example/station7/Main.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) {
         int[] itemPrices = { 1200, 2300, 3500, 2500, 1000, 2980, 9800, 4600, 5000, 2000 };
         // ここから
+        int all = 0;
+        for (int i = 0; i < itemPrices.length; i = i + 1){
+            all = all + itemPrices[i];
+        }
+        System.out.println(all);
         // ここまで
     }
 }
